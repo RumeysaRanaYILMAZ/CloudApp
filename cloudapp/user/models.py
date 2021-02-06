f4eba05ad1fa24dc74451c869c6b5c5a09c8fa01
Kenan Gülol <kenan.gulol1@protonmail.com>
2021-02-07 02:51:34 +0300

    DB models created and deployed

--- cloudapp/user/models.py
+++ cloudapp/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class SysUser(AbstractUser):
+    is_organizer = models.BooleanField(db_column='ISORGANIZER', default=False)
+
+    def __str__(self) -> str:
+        return self.username
+
+    class Meta:
+        db_table = "SYSUSER"
