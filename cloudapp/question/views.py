65075d0087f7ee84ae7e100e1a83972b0762639b
Kenan Gülol <kenan.gulol1@protonmail.com>
2021-02-13 02:24:51 +0300

    questioncontroller deleted and listing questions directed to examcontroller

--- cloudapp/question/views.py
+++ cloudapp/question/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .controllers import QuestionController
+from exam.controllers import ExamController
+
 
 def question_view(request):
 
-    questions=QuestionController.questions(1)
-    return render(request,'solvee.html', {'questions':questions})
+    questions = ExamController('EXM2021020000').questions()
+    return render(request, 'solvee.html', {'questions': questions})
